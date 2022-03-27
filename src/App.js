e3571d986868c5de08853f4e5797ba357af91e1a
Ghassen <ghassen.medyouni@gmail.com>
2022-03-28 00:15:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import {start} from "./Confetti";
+import shuffle from "shuffle-array";
 
-function App() {
+
+
+
+function Confetti(){
+  useEffect(() => {
+    start();
+  });
+  return <canvas id="canvas" />
+}
+
+function Tile({ id, children, onToggle , isSet}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div onClick={onToggle} className={`tile ${isSet ? "tile--set" : ""}`}>
+        {children}
+      </div>
   );
 }
+
+const aaa = [
+  "Great success",
+  "User engagement",
+  "Kodiak",
+  "Huge kudos to X",
+  "Suboptimal",
+  "Learning experience ",
+  "Personalized learning",
+  "Super excited ",
+  "Funnel",
+  "OKRs",
+  "Highest company priority ",
+  "It’s only a test",
+  "Operate like a startup",
+  "Keeping the momentum",
+  "The results look promising",
+  "Initial signals",
+  "Can’t wait to share results ",
+  "Significant increase ",
+  "High quality content",
+  "Keep product consistent ",
+  "Data driven ",
+  "Glorious X team",
+  "Allocate resources ",
+  "Alignment between X and Y",
+  "Happy to announce"
+];
+
+
+const data = shuffle(aaa).reduce(
+    (data, value, index) => ({ ...data, [index]: value}),
+    {}
+);
+
+
+function App() {
+  const [state, setState]  = useState({ checked: {} });
+  const isWon = checked => {
+    const range = [0, 1, 2, 3, 4];
+    return (
+        undefined !== range.find(row => range.every(column => checked[row * 5 + column])) ||
+        undefined !== range.find(column => range.every(row => checked[row * 5 + column])) ||
+
+        range.every(index => checked[index * 5 + index]) ||
+        range.every(index => checked[index * 5 + 4 - index])
+    );
+  };
+
+  const toggle = id =>
+    setState(state => {
+      const checked = { ...state.checked, [id]: !state.checked[id] };
+      const won = isWon(checked);
+      return {
+        ...state,
+        checked,
+        won
+      };
+    });
+
+    return (
+        <div className="App">
+          <h1>Bingo</h1>
+          <div className="wrapper">
+            {Object.keys(data).map(id => (
+                <Tile key={id}
+                      id={id}
+                      isSet={!!state.checked[id]}
+                      onToggle={() => toggle(id)}>
+
+                  {data[id]}
+                </Tile>
+            ))}
+          </div>
+          {state.won ? <Confetti/> : null}
+        </div>
+    );
+  }
+
 
 export default App;
