84cca6ef11e9feed03ff78206129d42699a237b6
Ghassen <ghassen.medyouni@gmail.com>
2022-03-28 00:31:56 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ function App() {
 
     return (
         <div className="App">
+
           <h1>Bingo</h1>
           <div className="wrapper">
             {Object.keys(data).map(id => (
@@ -93,9 +94,15 @@ function App() {
                   {data[id]}
                 </Tile>
             ))}
+
+
           </div>
+
           {state.won ? <Confetti/> : null}
+
+
         </div>
+
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
